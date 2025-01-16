5cec2968dce0d26cbc473a2de26137ec247984f1
oxotnik44 <artemnovikov1969@gmail.com>
2025-01-16 17:55:57 +0700

    api replacement 2

--- src/shared/api/http/axios-instance.ts
+++ src/shared/api/http/axios-instance.ts
@@ -1,2 +1,2 @@
-export const API_URL = "http://185.135.83.79/api/";
-export const API_URL_CLIENT = "http://185.135.83.79/api/";
+export const API_URL = "http://185.135.83.79:3000/api/";
+export const API_URL_CLIENT = "http://185.135.83.79:3000/api/";
